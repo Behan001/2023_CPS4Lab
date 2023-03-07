1380400ba2ebeb183a46b6c3c66ecde469588114
Behan001 <niallmoyvane@hotmail.com>
2023-03-07 00:16:38 +0000

    attempt

--- main.cpp
+++ main.cpp
@@ -4,10 +4,62 @@
 
 #define PI 3.14159265358979323846
 
+class Shape {
 
+    enum Shape_is{square, triangle, circle};
+public:
+//constructor
+    Shape(int a){
+        _a = a;
+        Shape_type = square;
+    }
+
+    Shape(int a, int b){
+        _a = a;
+        _b = b;
+        if(_b == 0)
+            Shape_type = circle;
+        else
+            Shape_type = triangle;
+    }
+
+    float area(){
+        if (Shape_type == circle)
+            return(PI*_a*_a);
+        if (Shape_type == triangle)
+           return(_a*_b*0.5);
+        if (Shape_type == square)
+            return(_a*_a);
+    }
+
+    void print_Shape(){
+        if (Shape_type == circle)
+            printf("Shape is a circle\r\n");
+        if (Shape_type == triangle)
+            printf("Shape is a triangle\r\n");
+        if (Shape_type == square)
+            printf("Shape is a square\r\n");
+    }
+private:
+    int _a;
+    int _b;
+    Shape_is Shape_type;
+
+};
 
 
 int main() {  
-   
+   //input
+   Shape square(5);
+   Shape triangle(3,1);
+   Shape circle(6, 0);
+
+   square.print_Shape();
+        printf("area is %f\r\n", square.area());
+   triangle.print_Shape();
+            printf("area is %f\r\n", triangle.area());
+
+   circle.print_Shape();
+           printf("area is %f\r\n", circle.area());
 
 }
